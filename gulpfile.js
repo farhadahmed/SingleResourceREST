7bfe79afdfa845c21d3de1abbb2ff58734aaf5ea
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-08-04 22:39:23 -0700

    Get p to display in view using angular

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var stylish = require('jshint-stylish');
+var webpack = require('gulp-webpack');
 
 gulp.task('jshint', function() {
   return gulp.src(['*.js','./lib/models/*.js', './test/*.js'])
@@ -13,3 +14,21 @@ gulp.task('test', function() {
   return gulp.src('test/*.js')
              .pipe(mocha( { reporter: 'nyan' } ));
 });
+
+gulp.task('webpack:dev', function() {
+  return gulp.src('app/js/client.js')
+             .pipe(webpack({
+               output: {
+                 filename: 'bundle.js'
+               }
+             }))
+             .pipe(gulp.dest('build/'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src('app/**/*.html')
+             .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['webpack:dev', 'copy']);
+gulp.task('default', ['build']);
